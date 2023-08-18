81214f2b0da4c5cb30fb3119ef97c12cb22a31e6
chiaWen <killerplay13@gmail.com>
2023-08-18 22:49:24 +0800

    [groupcreate] 修改groupverify

--- src/main/java/tw/com/cha102/groupcreate/model/GroupVerifyVO.java
+++ src/main/java/tw/com/cha102/groupcreate/model/GroupVerifyVO.java
@@ -5,12 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+
+import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
