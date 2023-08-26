5b2681da4e02200a69f3f7421779886b43afc98c
chiaWen <killerplay13@gmail.com>
2023-08-26 09:37:19 +0800

    [groupreport] 修改util 新增VO DAO service servlet

--- src/main/java/tw/com/cha102/groupreport/controller/GroupReportServlet.java
+++ src/main/java/tw/com/cha102/groupreport/controller/GroupReportServlet.java
@@ -1,5 +1,43 @@
 package tw.com.cha102.groupreport.controller;
 
-public class GroupReportServlet {
+import tw.com.cha102.groupreport.model.GroupReportDAO;
+import tw.com.cha102.groupreport.model.GroupReportDAOImpl;
+import tw.com.cha102.groupreport.model.GroupReportVO;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class GroupReportServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String action = req.getParameter("action");
+
+        if("getAll".equals(action)) {
+
+            GroupReportDAOImpl dao = new GroupReportDAOImpl();
+            List<GroupReportVO> list = dao.getAll();
+
+            HttpSession session = req.getSession();
+            session.setAttribute("list", list);
+
+            String url = "/listAll_GROUP_REPORT.jsp";
+            RequestDispatcher successView = req.getRequestDispatcher(url);
+            successView.forward(req, resp);
+            return;
+        }
+
+    }
 }
--- src/main/java/tw/com/cha102/groupreport/model/GroupReportDAOImpl.java
+++ src/main/java/tw/com/cha102/groupreport/model/GroupReportDAOImpl.java
@@ -3,8 +3,10 @@ package tw.com.cha102.groupreport.model;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class GroupReportDAOImpl implements GroupReportDAO{
+    //要問老師一下我把util傳上git會怎麼樣嗎?
     static {
         try {
             Class.forName(Util.DRIVER);
@@ -19,14 +21,16 @@ public class GroupReportDAOImpl implements GroupReportDAO{
         PreparedStatement pstmt = null;
         try {
             con = DriverManager.getConnection(Util.URL, Util.USER,Util.PASSWORD);
-            pstmt = con.prepareStatement("INSERT INTO GROUP_REPORT VALUES(?, ?, ?, ?, ?, ?)");
+            pstmt = con.prepareStatement("INSERT INTO GROUP_REPORT VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
 
             pstmt.setInt(1,groupReportVO.getGroupId());
             pstmt.setInt(2,groupReportVO.getGroupReportId());
             pstmt.setInt(3,groupReportVO.getReportedMemberId());
             pstmt.setInt(4,groupReportVO.getGroupId());
-            pstmt.setInt(5,groupReportVO.getEmployeeId());
-            pstmt.setInt(6,groupReportVO.getGroupReportStatus());
+            pstmt.setString(5,groupReportVO.getReportReason());
+            pstmt.setInt(6,groupReportVO.getEmployeeId());
+            pstmt.setInt(7,groupReportVO.getGroupReportStatus());
+            pstmt.setString(8,groupReportVO.getRejectReason());
             pstmt.executeUpdate();
         }catch(SQLException se) {
             se.printStackTrace();
@@ -37,6 +41,25 @@ public class GroupReportDAOImpl implements GroupReportDAO{
 
     @Override
     public void update(GroupReportVO groupReportVO) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = DriverManager.getConnection(Util.URL, Util.USER,Util.PASSWORD);
+            pstmt =con.prepareStatement("UPDATE GROUP_REPORT SET REPORT_MEMBER_ID=?,REPORTED_MEMBER_ID=?,GROUP_ID=?,REPORT_REASON=?,EMPLOYEE_ID=?,GROUP_REPORT_STATUS=?,REJECT_REASON=? WHERE GROUP_REPORT_ID=?");
+
+            pstmt.setInt(1,groupReportVO.getGroupId());
+            pstmt.setInt(2,groupReportVO.getGroupReportId());
+            pstmt.setInt(3,groupReportVO.getReportedMemberId());
+            pstmt.setInt(4,groupReportVO.getGroupId());
+            pstmt.setString(5,groupReportVO.getReportReason());
+            pstmt.setInt(6,groupReportVO.getEmployeeId());
+            pstmt.setInt(7,groupReportVO.getGroupReportStatus());
+            pstmt.setString(8,groupReportVO.getRejectReason());
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } finally {
+            Util.closeResources(con,pstmt,null);
+        }
 
     }
 
@@ -58,6 +81,7 @@ public class GroupReportDAOImpl implements GroupReportDAO{
                 groupReport.setReportMemberId(rs.getInt("REPORT_MEMBER_ID"));
                 groupReport.setReportedMemberId(rs.getInt("REPORTED_MEMBER_ID"));
                 groupReport.setGroupId(rs.getInt("GROUP_ID"));
+                groupReport.setReportReason(rs.getString("REPORT_REASON"));
                 groupReport.setEmployeeId(rs.getInt("EMPLOYEE_ID"));
                 groupReport.setGroupReportStatus(rs.getInt("GROUP_REPORT_STATUS"));
                 groupReport.setRejectReason(rs.getString("REJECT_REASON"));
@@ -68,7 +92,40 @@ public class GroupReportDAOImpl implements GroupReportDAO{
         }finally {
             Util.closeResources(con, pstmt, rs);
         }
-
         return groupReportList;
+    }
+
+//複合查詢，視情況補上
+    @Override
+    public List<GroupReportVO> getAll(Map<String, String[]> map) {
+        List<GroupReportVO> list = new ArrayList<GroupReportVO>();
+        GroupReportVO groupReport = null;
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = DriverManager.getConnection(Util.URL,Util.USER ,Util.PASSWORD);
+            String finalSQL = "SELECT * FROM GROUP_REPORT"+Util.getWhereCondition(map)+"ORDER BY GROUP_REPORT_ID";
+            pstmt = con.prepareStatement(finalSQL);
+            rs = pstmt.executeQuery();
+            while(rs.next()) {
+                groupReport = new GroupReportVO();
+                groupReport.setGroupReportId(rs.getInt("GROUP_REPORT_ID"));
+                groupReport.setReportMemberId(rs.getInt("REPORT_MEMBER_ID"));
+                groupReport.setReportedMemberId(rs.getInt("REPORTED_MEMBER_ID"));
+                groupReport.setGroupId(rs.getInt("GROUP_ID"));
+                groupReport.setReportReason(rs.getString("REPORT_REASON"));
+                groupReport.setEmployeeId(rs.getInt("EMPLOYEE_ID"));
+                groupReport.setGroupReportStatus(rs.getInt("GROUP_REPORT_STATUS"));
+                groupReport.setRejectReason(rs.getString("REJECT_REASON"));
+                list.add(groupReport);
+            }
+
+        } catch(SQLException se) {
+            se.printStackTrace();
+        }finally {
+            Util.closeResources(con, pstmt, rs);
+        }
+        return null;
     }
 }
