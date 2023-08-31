2c21c25d74475a4e80c71f0889bd1119f817e07b
Jasonddee <134155009+Jasonddee@users.noreply.github.com>
2023-08-31 17:46:05 +0800

    [shop] 商城功能結帳 訂單完成退貨 完成 (綠界部分先稍後 先用跳轉)

--- src/main/java/tw/com/cha102/order/controller/FrontendOrder.java
+++ src/main/java/tw/com/cha102/order/controller/FrontendOrder.java
@@ -7,6 +7,9 @@ import tw.com.cha102.order.model.entity.OrderVO;
 import tw.com.cha102.order.service.OrderService;
 import tw.com.cha102.product.model.entity.ProductVO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/oredr")
@@ -33,5 +36,58 @@ public class FrontendOrder {
             vo.setMessage("結帳失敗");
             return vo;
         }
+    }
+
+    //依訂單狀態刷新
+    @GetMapping("/limit3/{value}/{orderStatus}")
+    public List<OrderVO> getOrderByStatusLimit3(@PathVariable Integer value,@PathVariable int orderStatus){
+        List<OrderVO> list = new ArrayList<OrderVO>();
+        list=service.findByStatusTo3(value,orderStatus);
+        return list;
+    }
+
+    //前台依訂單數量 分頁
+    @GetMapping("/sortPage")
+    public int orderTotalCountByOrderStatus(@RequestParam Integer orderStatus){
+        return service.findOrderCountByOrderStatus(orderStatus);
+    }
+    //前台完成訂單
+    @PutMapping("/confirm/{orderId}")
+    public  OrderVO orderToReturn(@PathVariable Integer orderId){
+        OrderVO orderVO=new OrderVO();
+        if(service.confirmOrder(orderId)==true){
+            orderVO.setMessage("操作成功");
+            orderVO.setSuccessful(true);
+        }else{
+            orderVO.setMessage("操作失敗");
+            orderVO.setSuccessful(false);
+        }
+        return orderVO;
+    }
+
+    @PutMapping("/point/{memberId}/{usePoints}")
+    public Member memberUsePoints(@PathVariable Integer memberId, @PathVariable Integer usePoints){
+        Member member=new Member();
+        if(service.reduceMemberPoint(memberId,usePoints)==true){
+            member.setMessage("操作成功");
+            member.setSuccessful(true);
+        }else{
+            member.setMessage("操作失敗");
+            member.setSuccessful(false);
+        }
+        return member;
+    }
+
+    @PutMapping("/addpoint/{memberId}/{orderId}")
+    public Member memberAddPoints(@PathVariable Integer memberId, @PathVariable Integer orderId){
+        Member member=new Member();
+        if(service.addMemberPoint(memberId,orderId)==true){
+            member.setMessage("操作成功");
+            member.setSuccessful(true);
+        }else{
+            member.setMessage("操作失敗");
+            member.setSuccessful(false);
+        }
+        return member;
     }
 }
