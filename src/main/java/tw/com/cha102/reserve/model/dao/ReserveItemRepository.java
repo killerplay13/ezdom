17ef7a78ec0fe740465ab2d4223948b86c594a80
Jasonddee <134155009+Jasonddee@users.noreply.github.com>
2023-09-06 01:10:33 +0800

    [reserve] 教練日曆功能完成

--- src/main/java/tw/com/cha102/reserve/model/dao/ReserveItemRepository.java
+++ src/main/java/tw/com/cha102/reserve/model/dao/ReserveItemRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @Repository
 public interface ReserveItemRepository extends JpaRepository <ReserveItemVO, Integer> {
-    List<ReserveItemVO> findByCoachId(Integer coachId);
+    List<ReserveItemVO> findByCoachIdAndReserveItemStatus(Integer coachId,byte reserveItemStatus);
 
     List<ReserveItemVO> findByReserveItemStatusAndCoachId(byte reserveItemStatus,Integer ccoachId);
 
@@ -24,7 +24,7 @@ public interface ReserveItemRepository extends JpaRepository <ReserveItemVO, Int
 
     @Query(value = "SELECT RE.RESERVE_ID as reserveId ,RE.RESERVE_DATE as reserveDate,RE.RESERVE_TIME as reserveTime,"+
             "RE.ORDER_STATUS as orderStatus,RI.RESERVE_ITEM as reserveItem,RI.PLACE as place,RI.CONTENT as content,"+
-            "RI.AMOUNTS as amounts,CM.NICKNAME as nickname FROM reserve RE LEFT JOIN reserve_item RI ON RE.RESERVE_ITEM_ID"+
+            "RI.AMOUNTS as amounts,CM.NICKNAME as nickname,CM.COACH_ID as coachId FROM reserve RE LEFT JOIN reserve_item RI ON RE.RESERVE_ITEM_ID"+
             "=RI.RESERVE_ITEM_ID LEFT JOIN COACH_MEMBER CM ON CM.COACH_ID=RI.COACH_ID WHERE RE.MEMBER_ID= ?1 ORDER BY"+ " RE.RESERVE_ID DESC;",nativeQuery = true)
             List<ReserveItemDTO> getMemberReservationFormByMemberId(Integer memberId);
 }
