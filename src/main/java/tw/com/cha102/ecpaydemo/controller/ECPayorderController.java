88cba850a8e03f4246d1f702b9c9b5c24917e5f1
Jasonddee <134155009+Jasonddee@users.noreply.github.com>
2023-09-07 19:09:32 +0800

    [order]  綠界功能 訂單資料串上去 ok

--- src/main/java/tw/com/cha102/ecpaydemo/controller/ECPayorderController.java
+++ src/main/java/tw/com/cha102/ecpaydemo/controller/ECPayorderController.java
@@ -3,8 +3,11 @@ package tw.com.cha102.ecpaydemo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import tw.com.cha102.ecpaydemo.service.ECPayorderService;
+import tw.com.cha102.order.model.entity.OrderVO;
+
 @CrossOrigin(origins = "*")
 @RestController
 public class ECPayorderController {
@@ -13,9 +16,11 @@ public class ECPayorderController {
     ECPayorderService orderService;
 
     @PostMapping("/ecpayCheckout")
-    public String ecpayCheckout() {
-        String aioCheckOutALLForm = orderService.ecpayCheckout();
-
+    public String ecpayCheckout(@RequestBody OrderVO orderVO) {
+        String aioCheckOutALLForm = orderService.ecpayCheckout(orderVO);
+        if(aioCheckOutALLForm == null){
+            return "綠界付款失敗";
+        }
         return aioCheckOutALLForm;
     }
 }
