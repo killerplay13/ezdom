ec1fa5d38894d89e48a1f24e6143f09249b72584
YuChia0311 <jo1397200@gmail.com>
2023-09-07 16:18:14 +0800

    Revert "[group]我的揪團 個人頁面映射會員資料"
    This reverts commit 82ce1c5cd434183313293a88be2b6467377ffd84.

--- src/main/java/tw/com/cha102/member/service/impl/MemberServiceImpl.java
+++ src/main/java/tw/com/cha102/member/service/impl/MemberServiceImpl.java
@@ -101,11 +101,6 @@ public class MemberServiceImpl implements MemberService {
         return memberRepository.findAll();
     }
 
-    @Override //我的揪團 映射會員資料需要
-    public Member findById(Integer memberId) {
-        return memberRepository.findById(memberId).get();
-    }
-
     /**
      * sha 256 加密
      */
